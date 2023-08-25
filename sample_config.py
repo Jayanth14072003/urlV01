055955edba9ce8ab03ab57e2bd7c88affbd38ea4
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-25 12:20:46 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -25,11 +25,11 @@
 class Config(object):
 
     # get a token from @BotFather
-    BOT_TOKEN = ""
+    BOT_TOKEN = "5872747581:AAH7_XPCOCEVfbgUhepjJWlcOmj8wjDTjBk"
 
     # Get these values from my.telegram.org
-    API_ID = 1234567
-    API_HASH = ""
+    API_ID = 3393749
+    API_HASH = "a15a5954a1db54952eebd08ea6c68b71"
 
     # No need to change
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -40,11 +40,11 @@ class Config(object):
     PROCESS_MAX_TIMEOUT = 3700
 
     # TG Ids
-    LOG_CHANNEL = -1007119867241
-    OWNER_ID = 1288398723
+    LOG_CHANNEL = -1001636376488
+    OWNER_ID = 1061576483
 
     # bot username without @
-    BOT_USERNAME = "AdvanceUrlUploaderBot"
+    BOT_USERNAME = "jn_url_v3_bot"
 
     # auth users
     AUTH_USERS = [OWNER_ID]
